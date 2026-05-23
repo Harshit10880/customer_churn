0dda801565558c53728dfc28cfea5ddea2a8c809
Harshit10880 <harshit.cha10@gmail.com>
2026-05-23 12:39:03 +0530

    added decisiontree

---
+++ decisiontree.py
@@ -0,0 +1,19 @@
+from common_imports import *
+
+model_dt = tree.DecisionTreeClassifier()
+model_dt.fit(X_train, y_train)
+
+predt_dt = model_dt.predict(X_test)
+print(f"Model accuracy: {accuracy_score(y_test, predt_dt):.2f}")
+print("\nDetailed Performance Report:")
+print(classification_report(y_test, predt_dt))
+
+# support vector machine
+
+model_svm = SVC()
+model_svm.fit(X_train, y_train)
+
+predt_svm = model_svm.predict(X_test)
+print(f"Model accuracy: {accuracy_score(y_test, predt_svm):.2f}")
+print("\nDetailed Performance Report:")
+print(classification_report(y_test, predt_svm))
